96b3834fe163eb1f01617885b18111d164e9db40
Thriveni <veniperur@gmail.com>
2022-12-02 13:26:20 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{uesState, useState} from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [input,setInput]=uesState("");
+  const [result,setResult]=useState(0);
+  const handler = e =>{
+    setInput(e.target.value);
+  }
+  return (
+    <div>
+      <center>
+      <input type="text" value={input} name="input" onChange={handler}/>
+      <br/>
+     <button onClick={() => setResult(eval(input))}>Result</button>
+     <h4>Result is : {result}</h4>
+     <button onClick={() => setInput(input+'1')}>1</button>
+     <button onClick={() => setInput(input+'2')}>2</button>
+     <button onClick={() => setInput(input+'3')}>3</button>
+     <button onClick={() => setInput(input+'4')}>4</button>
+     <button onClick={() => setInput(input+'5')}>5</button><br/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+     <button onClick={() => setInput(input+'6')}>6</button>
+     <button onClick={() => setInput(input+'7')}>7</button>
+     <button onClick={() => setInput(input+'8')}>8</button>
+     <button onClick={() => setInput(input+'9')}>9</button>
+     <button onClick={() => setInput(input+'0')}>0</button><br/>
+
+     <button onClick={() => setInput(input+'+')}>+</button>
+     <button onClick={() => setInput(input+'-')}>-</button>
+     <button onClick={() => setInput(input+'*')}>*</button>
+     <button onClick={() => setInput(input+'/')}>/</button>
+     <button onClick={() => setInput('')}>clr</button><br/>
+      </center>
+      
+    </div>
+  )
+}
+export default App;
